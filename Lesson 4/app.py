1a0840f10424650fabc2bb4d1cda34b69ed44b45
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-05-29 17:10:45 -0300

    Lessons 4 to 9

---
+++ Lesson 4/app.py
@@ -0,0 +1,8 @@
+phrase = "Giraffe Academy"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("A"))
+print(phrase.replace("Giraffe", "Dog"))
