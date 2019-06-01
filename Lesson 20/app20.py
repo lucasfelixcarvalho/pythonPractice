64437ff55f62e15b18ebaa6a5c0662ca8986ac14
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-06-01 17:39:31 -0300

    Lessons 20 to 23
    Signed-off-by: Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>

---
+++ Lesson 20/app20.py
@@ -0,0 +1,6 @@
+friends = ["Jim", "Karen", "Kevin"]
+for index in range(len(friends)):
+    if index == 0:
+        print("First Iteration: " + friends[index])
+    else:
+        print(friends[index])
