46c9802181e2e23d72570df0bdd2c2d8b288e31a
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-06-09 22:28:11 -0300

    Created function to convert input from Layout to number. Changed division function
    Signed-off-by: Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>

--- Calculator/Operations.py
+++ Calculator/Operations.py
@@ -1,41 +1,62 @@
 def add(num1, num2):
-    result = float(num1) + float(num2)
-    if __return_float(num1) and __return_float(num2):
+    num1 = __convert_to_number(num1)
+    num2 = __convert_to_number(num2)
+
+    result = num1 + num2
+    if __return_float(num1) or __return_float(num2):
         return float(result)
     else:
         return int(result)
 
 
 def subtract(num1, num2):
-    result = float(num1) - float(num2)
-    if __return_float(num1) and __return_float(num2):
+    num1 = __convert_to_number(num1)
+    num2 = __convert_to_number(num2)
+
+    result = num1 - num2
+    if __return_float(num1) or __return_float(num2):
         return float(result)
     else:
         return int(result)
 
 
 def multiply(num1, num2):
-    result = float(num1) * float(num2)
-    if __return_float(num1) and __return_float(num2):
+    num1 = __convert_to_number(num1)
+    num2 = __convert_to_number(num2)
+
+    result = num1 * num2
+    if __return_float(num1) or __return_float(num2):
         return float(result)
     else:
         return int(result)
 
 
 def divide(num1, num2):
-    if int(num2) == 0:
+    try:
+        num1 = __convert_to_number(num1)
+        num2 = __convert_to_number(num2)
+
+        result = num1 / num2
+        return result
+    except ZeroDivisionError:
         return "Divide by zero not possible"
-    else:
-        result = float(num1) / float(num2)
-        return float(result)
 
 
 def change_signal(number):
+    number = __convert_to_number(number)
+
     if __return_float(number):
-        return float(number) * (-1)
+        return number * (-1)
     else:
-        return int(number) * (-1)
+        return number * (-1)
 
 
 def __return_float(number):
     return isinstance(number, float)
+
+
+def __convert_to_number(text_input):  # all inputs from the calculator comes as string, so we convert to number
+    if "." in text_input:
+        return float(text_input)
+    else:
+        return int(text_input)
