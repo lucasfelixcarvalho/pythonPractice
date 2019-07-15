72f152bf328faf4ea78a9ff05adfa4c6200f6f77
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-07-15 13:34:05 -0300

    Implemented delete function for product
    Change-Id: I2c4abe9754293757ae8f17fc3c500a7bc7d02399
    Signed-off-by: Lucas Carvalho <lucas.carvalho@landisgyr.com>

--- Free Code Camp Course/Django/src/products/views.py
+++ Free Code Camp Course/Django/src/products/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
@@ -62,3 +62,15 @@ def dynamic_lookup_view(request, id):
         "object": obj
     }
     return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('../../')
+    context = {
+        'object': obj
+    }
+    return render(request, 'products/product_delete.html', context)
--- Free Code Camp Course/Django/src/trydjango/urls.py
+++ Free Code Camp Course/Django/src/trydjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view, product_create_view2, product_create_view3, dynamic_lookup_view
+from products.views import product_detail_view, product_create_view, product_create_view2, product_create_view3, dynamic_lookup_view, product_delete_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -28,4 +28,5 @@ urlpatterns = [
     path('product/create2/', product_create_view2),
     path('product/create3/', product_create_view3),
     path('product/<int:id>/', dynamic_lookup_view, name='product'),
+    path('product/<int:id>/delete/', product_delete_view, name='product'),
 ]
