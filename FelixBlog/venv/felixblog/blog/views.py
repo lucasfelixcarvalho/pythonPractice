e2df67130c830a7ce0b0ccdc200ce1bad300b878
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-07-26 14:02:29 -0300

    Added blog first page and view. Installed bootstrap
    Change-Id: I2970d8097b634ec2c59653317bef35079b98d443

--- FelixBlog/venv/felixblog/blog/views.py
+++ FelixBlog/venv/felixblog/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from django.urls import reverse
+from django.views.generic import(
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+from .models import Article
 
 # Create your views here.
+class ArticleListView(ListView):
+    template_name = 'article/home.html'
+    queryset = Article.objects.all()
