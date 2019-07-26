abf42643c69adbe6e6f2a9e26da94c3ea53c1dc2
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-07-26 11:02:14 -0300

    Added blog app to django admin registration
    Change-Id: I69bf82507235a50977a55f9ac1f6c78620c9a2bc

--- FelixBlog/venv/felixblog/blog/models.py
+++ FelixBlog/venv/felixblog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    subtitle = models.CharField(max_length=60)
+    content = models.CharField(max_length=500)
+    postdate = models.DateTimeField(auto_now_add=True)
+    lastmodifieddate = models.DateTimeField(auto_now=True)
+    numberofhits = models.IntegerField(default=0)
