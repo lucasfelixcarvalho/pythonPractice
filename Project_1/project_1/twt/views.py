62fbf32026c2f7f16c3bb0c3344cbda91da79e57
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-12-28 19:21:52 -0300

    Default page

--- Project_1/project_1/twt/views.py
+++ Project_1/project_1/twt/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World! You're at the twt index page")
+    context = {}
+    return render(request, 'twt/index.html', context)
