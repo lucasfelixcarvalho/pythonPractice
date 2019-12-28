f91fee7db80dbeaa9d1bb5b95daf4b48f20de921
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-12-28 20:47:16 -0300

    Created initial page template and mock data

--- Project_1/project_1/twt/views.py
+++ Project_1/project_1/twt/views.py
@@ -4,5 +4,20 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    context = {}
+    tweets = []
+
+    if request.POST:
+        postData = request.POST.dict()
+        print(f"account_name: {postData.get('account_name')}")        
+        tweets.append(tweet('here is one tweet'))
+    else:
+        print("NOT POST")
+
+    context = {
+        'tweets': tweets
+    }
     return render(request, 'twt/index.html', context)
+
+class tweet:  
+    def __init__(self, text):  
+        self.text = text
