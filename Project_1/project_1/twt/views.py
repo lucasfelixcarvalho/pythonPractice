cc03db4902fb3a38a17e09627b1018b42df7ea6f
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-12-28 23:33:38 -0300

    Build call to Twitter api and got results back

--- Project_1/project_1/twt/views.py
+++ Project_1/project_1/twt/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
+import twitter
 
 # Create your views here.
 def index(request):
@@ -10,7 +11,8 @@ def index(request):
     if request.POST:
         postData = request.POST.dict()
         account_name = postData.get('account_name')
-        tweets.append(tweet('here is one tweet'))
+        raw_tweets = get_raw_tweets(account_name)
+        tweets = get_clean_tweets(raw_tweets)
     else:
         print("NOT POST")
 
@@ -24,5 +26,28 @@ class tweet:
     def __init__(self, text):  
         self.text = text
 
-def get_tweets(account_name):
+def get_raw_tweets(account_name):
     print(f'Searching last 5 tweets for account: {account_name}')
+    api = twitter.Api(consumer_key='',
+        consumer_secret='',
+        access_token_key='',
+        access_token_secret='')
+
+    if api.VerifyCredentials():
+        print('Credentials are valid')
+    else:
+        print('Invalid credentials')
+        return
+
+    results = api.GetUserTimeline(screen_name=account_name, count=2, exclude_replies=True)
+
+    print(f'Searched. Results count: {len(results)}')
+    return results
+
+def get_clean_tweets(raw_tweets):
+    tweets = []
+    for row in raw_tweets:
+        tweets.append(row.text)
+        print(row.text)
+
+    return tweets
