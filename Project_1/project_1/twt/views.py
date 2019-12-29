5d46f4de8c31ba291fd2a3c5ef9b83f93cb68699
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2019-12-28 23:03:42 -0300

    Finished skeleton of page to retrieve tweets

--- Project_1/project_1/twt/views.py
+++ Project_1/project_1/twt/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.template import loader
 
 # Create your views here.
 def index(request):
     tweets = []
+    account_name = None
 
     if request.POST:
         postData = request.POST.dict()
-        print(f"account_name: {postData.get('account_name')}")        
+        account_name = postData.get('account_name')
         tweets.append(tweet('here is one tweet'))
     else:
         print("NOT POST")
 
     context = {
-        'tweets': tweets
+        'tweets': tweets,
+        'account_name': account_name
     }
     return render(request, 'twt/index.html', context)
 
 class tweet:  
     def __init__(self, text):  
         self.text = text
+
+def get_tweets(account_name):
+    print(f'Searching last 5 tweets for account: {account_name}')
